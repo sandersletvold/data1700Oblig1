fa9037490c9f00c6ac2b53224e3fdb2f06cbd19a
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-02-06 21:34:10 +0100

    Fullført oblig 1.

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,68 @@
+// Deklarerer arrayet globalt, slik at slettBilletter funksjonen har tilgang til arrayet.
+let billettlagring = [];
+
+function kjopBilletter() {
+    // Henter inputene fra HTML siden
+    let film = document.getElementById("velgfilm").value;
+    let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value;
+    let telefonnr = document.getElementById("telefonnr").value;
+    let epost = document.getElementById("epost").value;
+
+    // Deklarerer et array og definer verdiene til et objekt
+    let ordre = {
+        film : film,
+        antall : antall,
+        fornavn : fornavn,
+        etternavn : etternavn,
+        telefonnr : telefonnr,
+        epost : epost
+    };
+
+    // Regler for validering
+    const navnRegex = /^[A-Za-z]+$/;
+    const telefonnrRegex = /^[0-9]+$/;
+    const epostRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    /*  "epostRegex" er hentet fra:
+        https://emaillistvalidation.com/blog/email-validation-in-javascript-using-regular-expressions-the-ultimate-guide/
+    */
+
+    // Valideringen av alle feltene
+    let feilmld = "";
+    if (film === "") {
+        feilmld += "Feil vedrørende din ordre ved: Velg film:"+"<br>";
+    }
+    if (antall <= 0) {
+        feilmld += "Feil vedrørende din ordre ved: Antall"+"<br>";
+    }
+    if (!navnRegex.test(fornavn)) {
+        feilmld += "Feil vedrørende din ordre ved: Fornavn"+"<br>";
+    }
+    if (!navnRegex.test(etternavn)) {
+        feilmld += "Feil vedrørende din ordre ved: Etternavn"+"<br>";
+    }
+    if (!telefonnrRegex.test(telefonnr)) {
+        feilmld += "Feil vedrørende din ordre ved: Telefonnr"+"<br>";
+    }
+    if (!epostRegex.test(epost)) {
+        feilmld += "Feil vedrørende din ordre ved: Epost"+"<br>";
+    }
+
+    let ut = "";
+    if (feilmld !== "") {
+        document.getElementById("billettfelt").innerHTML = feilmld;
+    } else {
+        billettlagring.push(ordre);
+        for (let billett of billettlagring) {
+            ut += "Film: "+billett.film+"<br>"+"Antall: "+billett.antall+"<br>"+"Fornavn: "+billett.fornavn+"<br>"+
+                "Etternavn: "+billett.etternavn+"<br>"+"Telefonnr: "+billett.telefonnr+"<br>"+"Epost: "+billett.epost+"<br><br>";
+        }
+        document.getElementById("billettfelt").innerHTML = ut;
+    }
+}
+
+function slettBilletter() {
+    document.getElementById("billettfelt").innerHTML = "";
+    billettlagring.length = 0;
+}
